6874c9f7c1e3052522ff1c319fb36bcf125d9bcf
Torbjörn Einarson <torbjorn@einarssons.se>
2024-03-26 17:42:47 +0100

    fix: remove absl from 3.0 backport

--- packager/media/formats/mp2t/es_parser_teletext_unittest.cc
+++ packager/media/formats/mp2t/es_parser_teletext_unittest.cc
@@ -4,7 +4,6 @@
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
-#include <absl/log/log.h>
 #include <gtest/gtest.h>
 #include <functional>
 
